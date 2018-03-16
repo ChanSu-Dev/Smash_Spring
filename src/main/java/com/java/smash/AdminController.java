faa8d5287dbfe6116589b22726ee60172f5d9907
ChanSu <sophia@ChanSuui-MacBook-Pro.local>
2018-03-17 02:51:04 +0900

    관리자 Session기능 구현

---
+++ src/main/java/com/java/smash/AdminController.java
@@ -0,0 +1,53 @@
+package com.java.smash;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.support.RequestContextUtils;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+	
+	@RequestMapping("adminMain")
+	public String adminMain(HttpSession session, HttpServletRequest request, Model model) {
+		//FlashMap으로 넘어온 정보
+		Map<String, ?> map = RequestContextUtils.getInputFlashMap(request);
+		
+		if (map != null) {
+			String id = (String) map.get("id");
+			String pwd = (String) map.get("pwd");
+			
+			session.setAttribute("id", id);
+			session.setAttribute("pwd", pwd);
+		}		
+		
+		return "admin/admin_main";
+	}
+	
+	@RequestMapping(value = "adminDevice")
+	public String adminDevice(HttpServletRequest request, Model model) {
+		
+		// db연결해서 row넘겨주기
+		return "admin/admin_device";
+	}
+	
+	@RequestMapping(value = "adminDoctor")
+	public String adminDoctor(HttpServletRequest request, Model model) {
+		
+		// db연결해서 row넘겨주기
+		return "admin/admin_doctor";
+	}
+	
+	@RequestMapping(value = "adminConnection")
+	public String adminConnection(HttpServletRequest request, Model model) {
+		
+		// db연결해서 row넘겨주기
+		return "admin/admin_connection";
+	}
+}
--- src/main/java/com/java/smash/HomeController.java
+++ src/main/java/com/java/smash/HomeController.java
@@ -1,6 +1,8 @@
 package com.java.smash;
 
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -9,6 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.FlashMap;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 import com.java.smash.dao.AdminDao;
 import com.java.smash.dto.AdminDto;
@@ -38,22 +43,27 @@ public class HomeController {
 	}
 	
 	@RequestMapping(value = "/loginCheck", method = RequestMethod.POST)
-	public String loginCheck(HttpServletRequest request, Model model) {
-		String Uid = (String) request.getParameter("id");
-		String Upwd = (String) request.getParameter("pwd");
+	public String loginCheck(HttpServletRequest request, Model model, RedirectAttributes redirectAttr) {
+		String id = (String) request.getParameter("id");
+		String pwd = (String) request.getParameter("pwd");
 		
-		if (Uid.equals(adminId) && Upwd.equals(adminPwd)) {
-			model.addAttribute("id", Uid);
-			model.addAttribute("pwd", Upwd);
-			return "/admin/admin";
+		//FlashMap을 이용해서 redirect하면서 값을 넘김
+		FlashMap fm = RequestContextUtils.getOutputFlashMap(request);
+		fm.put("id", id);
+		fm.put("pwd", pwd);
+		
+		if (id.equals(adminId) && pwd.equals(adminPwd)) {
+			redirectAttr.addFlashAttribute("id", id).addFlashAttribute("pwd", pwd);
+			return "redirect:admin/adminMain";
 		} 
 		//else if() { db의 사용자와 일치하는 경우	
 		//}
 		else {	// 사용자와 관리자에 일치하지 않는경우
 			return "redirect:home";
 		}
-		
 	}
+	
+	
 	
 	
 }
